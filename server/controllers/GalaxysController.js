89088268d45464409aa0048938203482f07d4c32
aaronpeet <aaronpeet@icloud.com>
2021-07-28 15:29:03 -0600

    galaxy controller done

--- server/controllers/GalaxysController.js
+++ server/controllers/GalaxysController.js
@@ -1,7 +1,7 @@
 import { galaxysService } from '../services/GalaxysService'
 import BaseController from '../utils/BaseController'
 
-export class StudentsController extends BaseController {
+export class GalaxysController extends BaseController {
   constructor() {
     super('api/galaxys')
     this.router
@@ -16,6 +16,42 @@ export class StudentsController extends BaseController {
     try {
       const galaxys = await galaxysService.getAll(req.query)
       res.send(galaxys)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const galaxy = await galaxysService.getById(req.params.id)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const galaxy = await galaxysService.create(req.body)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      const galaxy = await galaxysService.edit(req.body)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async destroy(req, res, next) {
+    try {
+      const galaxy = await galaxysService.destroy(req.params.id)
+      res.send({ message: 'Successfully Deleted' })
     } catch (error) {
       next(error)
     }
